6fa037cdcbcc1d3d157e4c630de7b963b84b152d
logickoder <chukwudumebiorazulike@gmail.com>
2023-04-03 14:26:28 +0100

    chore: initial commit

---
+++ example/build.gradle.kts
@@ -0,0 +1,69 @@
+@file:Suppress("DSL_SCOPE_VIOLATION", "UnstableApiUsage")
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "dev.logickoder.perfectfreehand"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "dev.logickoder.perfectfreehand"
+        minSdk = 23
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.compose.compiler.get()
+    }
+}
+
+dependencies {
+
+    implementation(libs.core)
+    implementation(libs.lifecycle)
+    implementation(libs.compose.activity)
+    implementation(platform(libs.compose))
+    implementation(libs.compose.ui)
+    implementation(libs.compose.ui.graphics)
+    implementation(libs.compose.ui.tooling.preview)
+    implementation(libs.compose.material3)
+
+    testImplementation(libs.junit)
+
+    androidTestImplementation(libs.junit.android)
+    androidTestImplementation(libs.espresso)
+    androidTestImplementation(platform(libs.compose))
+    androidTestImplementation(libs.compose.ui.test.junit4)
+
+    debugImplementation(libs.compose.ui.tooling)
+    debugImplementation(libs.compose.ui.test.manifest)
+}
---
+++ perfect-freehand/build.gradle.kts
@@ -0,0 +1,11 @@
+@file:Suppress("DSL_SCOPE_VIOLATION")
+
+plugins {
+    id("java-library")
+    alias(libs.plugins.kotlin.jvm)
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
